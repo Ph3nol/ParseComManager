b505f18651920891161f75953889538b63e4580c
Ph3nol <ph3@slynett.com>
2012-12-01 15:09:43 +0100

    First atoum test file added to make Travis happy!

---
+++ tests/units/Sly/ParseComManager/Manager/Manager.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace tests\units\Sly\ParseComManager\Manager;
+
+require_once __DIR__ . '/../../../../../vendor/autoload.php';
+
+use mageekguy\atoum;
+use Sly\ParseComManager\Manager\Manager as BaseManager;
+
+/**
+ * Manager.
+ *
+ * @uses atoum\test
+ * @author Cédric Dugat <cedric@dugat.me>
+ */
+class Manager extends atoum\test
+{
+}
